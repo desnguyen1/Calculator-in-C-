93fcb325a3d3e6f611c7f8827c32d13f616146c0
desnguyen1 <desnguyen1@gmail.com>
2020-07-22 16:09:50 -0400

    added continuous functionality

--- calculator.cpp
+++ calculator.cpp
@@ -3,53 +3,58 @@
 
 int main()
 {
-    float num1, num2, total;
+    float num2, total;
     int error = 0;
-    char op;
+    char op, choice;
     std::cout<<"\n---------------------------------------"<<std::endl;
     std::cout<<"Welcome to the basic calculator program";
     std::cout<<"\nPlease enter the first value: ";
-    std::cin>>num1;
-    std::cout<<"\nPlease enter the second value: ";
-    std::cin>>num2;
-
+    std::cin>>total;
     do{
-        std::cout<<"\nEnter the operation: ";
-        std::cin>>op;
-        //switch operator for basic calculator
-        switch(op){
-            case '+':
-                total = num1 + num2;
-                error = 0;
-                break;
-            case '-':
-                total = num1 + num2;
-                error = 0;
-                break;
-            case '/':
-                while(num2 == 0){
-                    std::cout<<"\nCannot divide by 0. Please choose another number: ";
-                    std::cin>>num2;
-                }
-                total = num1/num2;
-                error = 0;
-                break;
-            case '*':
-                total = num1 * num2;
-                error = 0;
-                break;
-            default:
-                std::cout<<"\nInvalid operation";
-                error = 1;
-                break;    
-        }
-    }while(error == 1);
+        do{
+            std::cout<<"\nPlease enter the next value: ";
+            std::cin>>num2;
+            std::cout<<"\nEnter the operation: ";
+            std::cin>>op;
+            //switch operator for basic calculator
+            switch(op){
+                case '+':
+                    total += num2;
+                    error = 0;
+                    break;
+                case '-':
+                    total -= num2;
+                    error = 0;
+                    break;
+                case '/':
+                    while(num2 == 0){
+                        std::cout<<"\nCannot divide by 0. Please choose another number: ";
+                        std::cin>>num2;
+                    }
+                    total /= num2;
+                    error = 0;
+                    break;
+                case '*':
+                    total *= num2;
+                    error = 0;
+                    break;
+                default:
+                    std::cout<<"\nInvalid operation";
+                    error = 1;
+                    break;    
+            }
+        }while(error == 1);
+        std::cout<<"Total is: "<<total<<std::endl;
+        std::cout<<"\nDo another operation? 'Y' or 'N': ";
+        std::cin>>choice;
+    }while(toupper(choice) == 'Y');
 
+    /*
     //test print
     std::cout<<"Num1 is: "<<num1<<std::endl;
     std::cout<<"Num2 is: "<<num2<<std::endl;
     std::cout<<"Operator is: "<<op<<std::endl;
-    std::cout<<"Total is: "<<total<<std::endl;
+    */
     
     return 0;
 }
